728adb61c0c80b36a8ce962cddc40eff437af922
sany0chekk <olexandr.veseluy@gmail.com>
2024-02-25 16:12:21 +0200

    Customer - Update swiper

--- src/main.js
+++ src/main.js
@@ -1,21 +1,37 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
-
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
   },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
+    dragSize: 135,
+  },
+  slidesPerView: 3,
+  spaceBetween: 30,
+  on: {
+    slideChange: function () {
+      // Знаходимо активний слайд
+      const activeSlide = this.slides[this.activeIndex];
+
+      // Знаходимо всі слайди
+      const allSlides = this.slides;
+
+      // Перебираємо всі слайди
+      allSlides.forEach(slide => {
+        if (slide === activeSlide) {
+          // Додаємо клас для активного слайду
+          slide.classList.add('active-slide');
+        } else {
+          // Видаляємо клас для неактивних слайдів
+          slide.classList.remove('active-slide');
+        }
+      });
+    },
   },
 });
