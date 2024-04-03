029e5acac9b93d5dc6132d90d2b51b3660626da7
Vikto0004 <vikto0004@gmail.com>
2024-04-03 10:26:22 +0200

    Update model.order.js

--- src/js/model.order.js
+++ src/js/model.order.js
@@ -84,24 +84,69 @@ const btnPushOnBasket = document.querySelector('.target-btn');
 const modelOrder = document.querySelector('.model-order-container');
 
 //пуш вибраного продукта в кошик
+const plusNewArr = []; //* масиви з кнопками
+const minusNewArr = [];
 const clonePush = () => {
   const modelOrderClone = modelOrder.cloneNode(true);
   basket.appendChild(modelOrderClone);
   backrop.style.opacity = '0';
+  minusNewArr.splice(0);
+  plusNewArr.splice(0);
   setTimeout(function () {
     backrop.style.display = 'none';
     document.body.style.overflow = 'auto';
   }, 500);
+  plusNewArr.push(basket.querySelectorAll('#plusProduct'));
+  minusNewArr.push(basket.querySelectorAll('#minusProduct'));
 };
 btnPushOnBasket.addEventListener('click', clonePush);
-
+console.log(minusNewArr);
 //відкриття модалки
-const clickOpenBasket = () => {
+const clickOpenBasket = event => {
   basketModal.style.display = 'block';
   document.body.style.overflow = 'hidden';
   setTimeout(function () {
     basketModal.style.opacity = '1';
   }, 50);
+  //* ---- додаємо кількість продуктів ---//
+  let add = 0;
+  const addProduct = event => {
+    const numberProduct =
+      event.currentTarget.parentElement.previousElementSibling.firstElementChild
+        .lastElementChild;
+    const priceProduct =
+      event.currentTarget.parentElement.previousElementSibling.lastElementChild
+        .lastElementChild;
+    if (add >= numberProduct.textContent) {
+      numberProduct.textContent = ++add;
+      const price =
+        (priceProduct.textContent.replace('$', '') / (add - 1)) * add;
+      priceProduct.textContent = '$' + parseFloat(price.toFixed(2)); //* Змінюємо вартість покупки
+    } else add = parseInt(numberProduct.textContent);
+    console.log(add);
+  };
+  //* перебираємо масив з кнопками
+  if (plusNewArr.length > 0) {
+    for (const element of plusNewArr[0]) {
+      element.addEventListener('click', addProduct);
+      console.log(element);
+    }
+  }
+  const subtractProduct = event => {
+    const numberProduct =
+      event.currentTarget.parentElement.previousElementSibling.firstElementChild
+        .lastElementChild;
+    console.log(numberProduct);
+    const priceProduct =
+      event.currentTarget.parentElement.previousElementSibling.lastElementChild
+        .lastElementChild;
+    console.log(priceProduct);
+  };
+  if (minusNewArr.length > 0) {
+    for (const element of minusNewArr[0]) {
+      element.addEventListener('click', subtractProduct);
+    }
+  }
 };
 openBasket.addEventListener('click', clickOpenBasket);
 
