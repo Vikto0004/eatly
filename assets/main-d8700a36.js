8fcd816f79adefba6b9177d8f9d1b0aba11bfc31
Vikto0004 <150432826+Vikto0004@users.noreply.github.com>
2024-05-02 17:18:59 +0000

    Deploying to gh-pages from @ Vikto0004/eatly@969e932a4404b7f6cca441a9925ecd7b4bae895e 🚀

---
+++ assets/main-d8700a36.js
@@ -0,0 +1,2 @@
+import"./model-order-9fb5319b.js";import"./header-5b526b3e.js";
+//# sourceMappingURL=main-d8700a36.js.map
--- assets/main-f717625b.js
+++
@@ -1,2 +0,0 @@
-import"./model-order-1f068d87.js";import"./header-969a1f5c.js";
-//# sourceMappingURL=main-f717625b.js.map
