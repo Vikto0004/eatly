fa75bb1d9d06280988f09032ea247e95e85c879b
Vikto0004 <150432826+Vikto0004@users.noreply.github.com>
2024-05-02 16:32:17 +0000

    Deploying to gh-pages from @ Vikto0004/eatly@3e414052f27c6d4f4b6919f80fb772ec20566b46 🚀

--- commonHelpers5.js
+++
@@ -1,2 +0,0 @@
-import"./assets/header-969a1f5c.js";/* empty css                      */import"./assets/purchases-animation-f7d38f55.js";import"./assets/main-f717625b.js";import{a as o}from"./assets/vendor-6ca51d60.js";import"./assets/model-order-1f068d87.js";setTimeout(()=>{o.init({disable:"mobile",duration:1700,offset:300,once:!0})},100);
-//# sourceMappingURL=commonHelpers5.js.map
