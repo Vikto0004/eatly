c41134453783ff84592e3e7c4b66bee7bc2b500a
Vikto0004 <150432826+Vikto0004@users.noreply.github.com>
2024-05-02 17:48:06 +0000

    Deploying to gh-pages from @ Vikto0004/eatly@33bc3e64cd6b5b2331b7abe4492bf895826792ca 🚀

---
+++ assets/main-a0ebce6a.js
@@ -0,0 +1,2 @@
+import"./model-order-d425a80b.js";import"./header-5b526b3e.js";
+//# sourceMappingURL=main-a0ebce6a.js.map
--- assets/main-d8700a36.js
+++
@@ -1,2 +0,0 @@
-import"./model-order-9fb5319b.js";import"./header-5b526b3e.js";
-//# sourceMappingURL=main-d8700a36.js.map
