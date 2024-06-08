217b649562207c502b4326af6a0be8c796c86ee1
Vikto0004 <150432826+Vikto0004@users.noreply.github.com>
2024-06-08 13:24:13 +0000

    Deploying to gh-pages from @ Vikto0004/eatly@92fb12f686cd204a9ee22204b560b337d91ce80d 🚀

---
+++ assets/main-0b7737ac.js
@@ -0,0 +1,2 @@
+import"./model-order-d425a80b.js";import"./header-be749d18.js";const e=document.querySelector(".scroll-btn ");let o=0,s=!0;window.addEventListener("scroll",()=>{let l=window.scrollY;l<o&&s?(e.classList.add("is-active-scroll"),s=!1):l>o&&!s?(e.classList.remove("is-active-scroll"),s=!0):l===0&&e.classList.remove("is-active-scroll"),o=l<=0?0:l});e.addEventListener("click",()=>{window.scrollTo({top:0,behavior:"smooth"}),e.classList.remove("is-active-scroll")});
+//# sourceMappingURL=main-0b7737ac.js.map
