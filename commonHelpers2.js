524df9001e85c41ffdcf06090b121f88a1c61001
Vikto0004 <150432826+Vikto0004@users.noreply.github.com>
2024-06-16 18:51:45 +0000

    Deploying to gh-pages from @ Vikto0004/eatly@c2c8b4095e0226e92b4041512a68d17acd2d78ac 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import"./assets/header-click-color-0ea842b7.js";import{e as h}from"./assets/scroll-up-f0ac5aba.js";import"./assets/header-608d5493.js";import"./assets/open-close-faqs-3cfc2c04.js";const u=t=>t.map(({author:s,images:e,publishedAt:o,title:x})=>`<li class="latest-articles-item">
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import"./assets/header-click-color-0ea842b7.js";import{e as v}from"./assets/scroll-up-f0ac5aba.js";import{r as u}from"./assets/mockapi-0a9f77fa.js";import"./assets/header-608d5493.js";import"./assets/open-close-faqs-3cfc2c04.js";const p=t=>t.map(({author:a,images:e,publishedAt:c,title:y})=>`<li class="latest-articles-item">
         <picture>
           <source
             srcset="
@@ -27,7 +27,7 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                
             alt="latest articles img"
           />
         </picture>
-        <h3 class="latest-articles-questions">${x}</h3>
+        <h3 class="latest-articles-questions">${y}</h3>
         <div class="latest-articles-wrap">
           <img
             srcset="
@@ -40,9 +40,9 @@ import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                
           />
           <div>
             <p class="latest-articles-written">Written By</p>
-            <p class="latest-articles-autor">${s}</p>
+            <p class="latest-articles-autor">${a}</p>
           </div>
-          <p class="latest-articles-date">${o}</p>
+          <p class="latest-articles-date">${c}</p>
         </div>
-      </li>`).join("");function m(t=1,r=3){const s=new URL("https://666d953c7a3738f7caccc458.mockapi.io/articles");return s.searchParams.append("page",t),s.searchParams.append("limit",r),fetch(s,{method:"GET",headers:{"content-type":"application/json"}}).then(e=>{if(!e.ok)throw new Error;return e.json()}).catch(e=>console.log(e))}const n=document.querySelector(".latest-articles-list-js"),a=document.querySelector(".latest-articles-more-js"),l=document.querySelector(".latest-articles-backdrop-js"),c=document.querySelector(".latest-articles-answer-js"),v=document.querySelector(".latest-articles-questions"),w=document.querySelector(".latest-articles-date"),L=document.querySelector(".latest-answer-btn-js");let i=1,y=3,k=3;const d=[];m(i,y).then(t=>{d.push(...t),n.innerHTML=u(t)}).catch(t=>console.log(t));a.addEventListener("click",()=>{a.classList.add("load"),a.disabled=!0,m(++i,y).then(t=>{a.classList.remove("load"),a.disabled=!1,n.insertAdjacentHTML("beforeend",u(t)),d.push(...t),window.scrollBy({top:400,behavior:"smooth"}),k===i&&(a.style.display="none")}).catch(t=>console.log(t))});n.addEventListener("click",t=>{if(t.currentTarget===t.target)return;const s=t.target.closest(".latest-articles-item").querySelector(".latest-articles-questions").textContent,{answer:e}=d.filter(({title:o})=>o===s)[0];v.textContent=s,w.textContent=e,f()});L.addEventListener("click",p);l.addEventListener("click",p);document.addEventListener("keydown",t=>{t.key==="Escape"&&p()});function f(){h.classList.remove("is-active-scroll"),l.style.display="block",c.style.display="flex",document.body.style.overflow="hidden",setTimeout(()=>{l.classList.add("latest-backdrop-active"),c.classList.add("latest-answer-active")})}function p(){l.classList.remove("latest-backdrop-active"),c.classList.remove("latest-answer-active"),setTimeout(()=>{l.style.display="none",c.style.display="none",document.body.style.overflow="auto"},300)}
+      </li>`).join(""),i=document.querySelector(".latest-articles-list-js"),s=document.querySelector(".latest-articles-more-js"),l=document.querySelector(".latest-articles-backdrop-js"),r=document.querySelector(".latest-articles-answer-js"),L=document.querySelector(".latest-articles-questions"),w=document.querySelector(".latest-articles-date"),h=document.querySelector(".latest-answer-btn-js");let o=1,m=3,k=3;const n=[];u(o,m).then(t=>{n.push(...t),i.innerHTML=p(t)}).catch(t=>console.log(t));s.addEventListener("click",()=>{s.classList.add("load"),s.disabled=!0,u(++o,m).then(t=>{s.classList.remove("load"),s.disabled=!1,i.insertAdjacentHTML("beforeend",p(t)),n.push(...t),window.scrollBy({top:400,behavior:"smooth"}),k===o&&(s.style.display="none")}).catch(t=>console.log(t))});i.addEventListener("click",t=>{if(t.currentTarget===t.target)return;const a=t.target.closest(".latest-articles-item").querySelector(".latest-articles-questions").textContent,{answer:e}=n.filter(({title:c})=>c===a)[0];L.textContent=a,w.textContent=e,b()});h.addEventListener("click",d);l.addEventListener("click",d);document.addEventListener("keydown",t=>{t.key==="Escape"&&d()});function b(){v.classList.remove("is-active-scroll"),l.style.display="block",r.style.display="flex",document.body.style.overflow="hidden",setTimeout(()=>{l.classList.add("latest-backdrop-active"),r.classList.add("latest-answer-active")})}function d(){l.classList.remove("latest-backdrop-active"),r.classList.remove("latest-answer-active"),setTimeout(()=>{l.style.display="none",r.style.display="none",document.body.style.overflow="auto"},300)}
 //# sourceMappingURL=commonHelpers2.js.map
