c2c8b4095e0226e92b4041512a68d17acd2d78ac
Vikto0004 <vikto0004@gmail.com>
2024-06-16 20:51:28 +0200

    added registration for user

--- src/js/create-account.js
+++ src/js/create-account.js
@@ -1,5 +1,8 @@
 import $ from 'jquery';
 import 'jquery-validation';
+import { receivingDataUser, savingDataUser } from './mockapi';
+
+const elSignUpEmail = document.querySelector('.sign-up-email');
 
 $(document).ready(function () {
   $('#sign-up-form').validate({
@@ -38,8 +41,47 @@ $(document).ready(function () {
       $(element).removeClass('sign-input-error');
     },
     submitHandler: function (form) {
-      alert('Form submitted successfully!');
-      form.submit();
+      const userName = form.elements.name.value;
+      const userEmail = form.elements.email.value;
+      const userPass = form.elements.password.value;
+
+      checkUserInBaseData(userEmail, userName, userPass);
     },
   });
 });
+
+function checkUserInBaseData(userEmail, userName, userPass) {
+  receivingDataUser()
+    .then(data => {
+      const res = data.find(({ email }) => email === userEmail);
+      if (res) {
+        const leLabel =
+          '<label class="error" for="email">This email is registered</label>';
+        elSignUpEmail.classList.add('sign-input-error');
+        elSignUpEmail.insertAdjacentHTML('afterend', leLabel);
+
+        elSignUpEmail.addEventListener('input', e => {
+          const elLableErr = e.target.nextElementSibling;
+          elLableErr.style.display = 'none';
+        });
+      } else {
+        savingUserData(userEmail, userName, userPass);
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+async function savingUserData(userEmail, userName, userPass) {
+  const userData = {
+    name: userName,
+    email: userEmail,
+  };
+  localStorage.setItem('userData', JSON.stringify(userData));
+
+  await savingDataUser({
+    name: userName,
+    email: userEmail,
+    password: userPass,
+  });
+  window.location.href = './';
+}
--- src/js/mockapi.js
+++ src/js/mockapi.js
@@ -13,3 +13,28 @@ export function receivingDataArticles(page = 1, limit = 3) {
     })
     .catch(error => console.log(error));
 }
+
+export function receivingDataUser() {
+  return fetch('https://666d953c7a3738f7caccc458.mockapi.io/users', {
+    method: 'GET',
+    headers: { 'content-type': 'application/json' },
+  })
+    .then(res => {
+      if (!res.ok) throw new Error();
+      return res.json();
+    })
+    .catch(error => console.log(error));
+}
+
+export function savingDataUser(postData) {
+  return fetch('https://666d953c7a3738f7caccc458.mockapi.io/users', {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(postData),
+  })
+    .then(res => {
+      if (!res.ok) throw new Error();
+      return res.json();
+    })
+    .catch(error => console.log(error));
+}
